db7fac1ce064dcb1aa272aed01ce62594e8b84d3
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-11-04 20:47:14 +0000

    Juntar implementações get_terminal_size em 1 file

--- src/term_size.c
+++ src/term_size.c
@@ -20,4 +20,24 @@ int get_terminal_size(TerminalSize *size)
     }
 }
 
+#elif __unix__
+
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+int get_terminal_size(TerminalSize *size)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 0)
+    {
+        size->rows = ws.ws_row;
+        size->columns = ws.ws_col;
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
 #endif
