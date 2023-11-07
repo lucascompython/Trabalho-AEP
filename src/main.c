5c07ac402a5576179bfb9f7b60d1bb80d099a419
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-11-07 13:33:12 +0000

    Garantir que o cursor fica visivel no final

--- src/main.c
+++ src/main.c
@@ -3,28 +3,40 @@
 #include "menu.h"
 #include "term_size.h"
 #include <locale.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void restoreCursor(int signal)
+{
+  printf("SIGNAL: %d", signal);
+  show_cursor();
+}
+
 TerminalSize term_size;
 
-void menu_gestao_clientes() {
+void menu_gestao_clientes()
+{
   menu_centered_item("Gestão de clientes", BLUE, BOLD, 0);
 }
-void menu_gestao_produtos() {
+void menu_gestao_produtos()
+{
   menu_centered_item("Gestão de produtos", BLUE, BOLD, 0);
 }
-void menu_gestao_vendas() {
+void menu_gestao_vendas()
+{
   menu_centered_item("Gestão de vendas", BLUE, BOLD, 0);
 }
-void menu_gestao_stocks() {
+void menu_gestao_stocks()
+{
   menu_centered_item("Gestão de stocks", BLUE, BOLD, 0);
 }
 
-void menu_principal(void) {
+void menu_principal(void)
+{
 
   char *options[] = {"Gestão de clientes", "Gestão de produtos",
                      "Gestão de vendas", "Gestão de stocks", "Sair"};
@@ -32,7 +44,8 @@ void menu_principal(void) {
   int32_t totalOptions = (int)LENGTH(options);
   int32_t result = arrow_menu(options, totalOptions);
   clear_menu();
-  switch (result) {
+  switch (result)
+  {
   case 0:
     menu_gestao_clientes();
     break;
@@ -54,7 +67,8 @@ void menu_principal(void) {
   }
 }
 
-void welcome_screen(void) {
+void welcome_screen(void)
+{
   clear_menu();
 
   menu_centered_item("Bem vindo ao sistema de gestão de stocks", GREEN, BOLD,
@@ -68,17 +82,23 @@ void welcome_screen(void) {
   menu_principal();
 }
 
-int main(void) {
+int main(void)
+{
   setlocale(LC_ALL, "Portuguese.UTF8");
 
   hide_cursor();
+  // restaure o cursor quando o programa terminar
+  signal(SIGINT, restoreCursor);  // Ctrl + C
+  signal(SIGTERM, restoreCursor); // kill
+  signal(SIGSEGV, restoreCursor); // Segmentation fault
 
-  if (get_terminal_size(&term_size) != 0) {
+  if (get_terminal_size(&term_size) != 0)
+  {
     fprintf(stderr, "Erro a conseguir o tamanho do terminal...\n");
     return 1;
   }
 
   welcome_screen();
-  show_cursor();
+  restoreCursor(0); // Restore cursor on exit
   return 0;
 }
