35a882958b8060468c940f8dcab39c03cf35fdfb
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-11-08 11:26:01 +0000

    Tirar alocação de memória

--- src/uuid.c
+++ src/uuid.c
@@ -8,11 +8,7 @@ char *uuid_gen(void)
 {
   uuid_t uuid;
   uuid_generate_random(uuid);
-  char *uuid_str = malloc(37);
-  if (uuid_str == NULL)
-  {
-    return NULL;
-  }
+  static char uuid_str[37];
   uuid_unparse(uuid, uuid_str);
   return uuid_str;
 }
